41bdf3eb1d4da2759eda2e14ff9ac1c1fa734f72
Andrea <andrea.bruera@gmail.com>
2023-08-01 21:50:51 +0100

    update

--- extract_static.py
+++ extract_static.py
@@ -126,7 +126,21 @@ else:
     with open(coocs_file, 'wb') as o:
         pickle.dump(final_coocs, o)
 
+total = sum([final_freqs[w] for w in conc_vocab])
+
 collector['count'] = {k : numpy.average([[final_coocs[vocab[k_word]][vocab[other]] for other in conc_vocab] for k_word in k.split()], axis=0) for k in full_stimuli}
+collector['count-ppmi'] = {k : numpy.average(
+                            [[
+                              #max(
+                                 numpy.log2(
+                                 (
+                                  max(final_coocs[vocab[k_word]][vocab[other]], 0.1) / total
+                                  ) / (
+                                       final_freqs[k_word] * (final_freqs[other]**0.75)
+                                 #      )
+                                 #          ), #0) 
+                                 )) for other in conc_vocab] for k_word in k.split()],
+                             axis=0) for k in full_stimuli}
 collector['count-log'] = {k : [numpy.log(v) if v!=0. else 0. for v in vec] for k, vec in collector['count'].items()}
 
 print('loaded!')
